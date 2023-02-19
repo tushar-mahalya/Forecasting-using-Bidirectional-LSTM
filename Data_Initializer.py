26c66efb3b44e54b71f3f556aa01aab25b585604
Tushar <tusharmahalya@gmail.com>
2023-02-19 15:26:35 +0000

    function optimization
    test function change + optimized for multi sequence data #13

--- Data_Initializer.py
+++ Data_Initializer.py
@@ -2,15 +2,24 @@ import numpy as np
 import pandas as pd
 
 
-def predictor_response_split(df: pd.DataFrame, window_size: int):
+def predictor_response_split(df: pd.DataFrame, window_size: int, multi_seq: bool = False):
     X = []
     y = []
-    numpy_df = df.to_numpy()
-    for i in range(len(numpy_df) - window_size):
-        row = [r for r in numpy_df[i:i + window_size]]
-        X.append(row)
-        label = [numpy_df[i + window_size][0]]
-        y.append(label)
+    if multi_seq:
+        numpy_df = df.to_numpy()
+        for i in range(len(numpy_df) - window_size):
+            row = [r for r in numpy_df[i:i + window_size]]
+            X.append(row)
+            label = [numpy_df[i + window_size][0]]
+            y.append(label)
+
+    else:
+        numpy_df = df[['normal_close']].to_numpy()
+        for i in range(len(numpy_df) - window_size):
+            row = [r for r in numpy_df[i:i + window_size]]
+            X.append(row)
+            label = numpy_df[i + window_size]
+            y.append(label)
 
     return np.array(X), np.array(y)
 
@@ -23,8 +32,8 @@ def train_test_split(X: np.array, y: np.array, split_size: float):
     return X_train, y_train, X_test, y_test
 
 
-def train_test_data(df: pd.DataFrame, split_size: float = 0.2, window_size: int = 7):
-    X, y = predictor_response_split(df, window_size)
+def train_test_data(df: pd.DataFrame, split_size: float = 0.2, window_size: int = 7, multi_seq: bool = False):
+    X, y = predictor_response_split(df, window_size, multi_seq)
     X_train, y_train, X_test, y_test = train_test_split(X, y, split_size)
 
     return X_train, y_train, X_test, y_test
