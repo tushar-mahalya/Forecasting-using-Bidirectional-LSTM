004d87ae8b4792db2d2c61df7f043b7042914b3b
tushar-mahalya <tusharmahalya@gmail.com>
2023-05-12 10:35:56 +0000

    modular code #3

--- src/utils.py
+++ src/utils.py
@@ -7,3 +7,34 @@ def normalize_feature(stock_df: pd.DataFrame, col: str):
     scaler.fit(values)
     scaled = scaler.transform(values)
     return list(scaled.flatten())
+
+def predictor_response_split(df: pd.DataFrame, window_size: int, seq: str = 'Simple'):
+    X = []
+    y = []
+    if seq == 'Multi':
+        numpy_df = df.to_numpy()
+        for i in range(len(numpy_df) - window_size):
+            row = [r for r in numpy_df[i:i + window_size]]
+            X.append(row)
+            label = [numpy_df[i + window_size][0]]
+            y.append(label)
+
+    elif seq == 'Simple':
+        numpy_df = df[['normal_close']].to_numpy()
+        for i in range(len(numpy_df) - window_size):
+            row = [r for r in numpy_df[i:i + window_size]]
+            X.append(row)
+            label = numpy_df[i + window_size]
+            y.append(label)
+    else:
+        print('Wrong Choice of sequence.\nType either \'Simple\' or \'Multi\' in \"seq\" attribute.')
+
+    return np.array(X), np.array(y)
+
+
+def train_test_split(X: np.array, y: np.array, split_size: float):
+    train_split = len(X) - int(len(X) * split_size)
+    X_train, y_train = X[:train_split], y[:train_split]
+    X_test, y_test = X[train_split:], y[train_split:]
+
+    return X_train, y_train, X_test, y_test
